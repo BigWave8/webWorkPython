4fe42bfe5c4abdb7af914fd4643309f8981219b9
BigWave8 <rostykchess@gmail.com>
2020-05-11 22:47:27 +0300

    added file with rest api and connetion to db for lab 12

--- main/models/abstract_stationery.py
+++ main/models/abstract_stationery.py
@@ -1,7 +1,7 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
-class AbstractStationery(ABC):
+class AbstractStationery():
 
     def __init__(self, price_in_hryvnia, color, producer, target_age):
         self.price_in_hryvnia = price_in_hryvnia
@@ -12,3 +12,10 @@ class AbstractStationery(ABC):
     def __str__(self):
         return "price: %d, color: %s, producer: %s, target age: %d" % (
             self.price_in_hryvnia, self.color, self.producer, self.target_age)
+
+    @abstractmethod
+    def hello_world(self):
+        raise NotImplementedError
+
+if __name__ == '__main__':
+    stationery = AbstractStationery(15, "red", "ua", 14)
